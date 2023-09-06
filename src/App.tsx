0cf3bd538afbe15d2c3ad1e5c11c96d2375560c1
cjsan <cjsanchez@live.com>
2023-09-06 13:29:04 +0200

    Create MSqrdColors.tsx and set doc's.body.bgColor

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import NavBar from "./NavBar";
 import React, { useRef } from "react";
 import { Services } from "./Services";
 import { Contact } from "./Contact";
+import MsqrdColors from "./MSqrdColors";
 
 export default function App() {
   const aboutRef    = React.useRef<HTMLDivElement>(null);
   const servicesRef = React.useRef<HTMLDivElement>(null);
   const contactRef  = React.useRef<HTMLDivElement>(null);
+  
+  document.body.style.backgroundColor = MsqrdColors.darkBlue;
 
   return (
     <>
---
+++ src/MSqrdColors.tsx
@@ -0,0 +1,8 @@
+
+export default class MsqrdColors{
+    static darkBlue = "#000033"
+    static lightBlue = "#46c7f4"
+    static pink = "#ed3b94"
+    static yellow = "#f7ec37"
+    static white = "#ffffff"
+}
